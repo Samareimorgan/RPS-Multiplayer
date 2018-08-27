51f5b18f0d1a2bd80c84722ff0ae0d305cb7a63c
Samantha Johnston <samareimorgan@gmail.com>
2018-08-27 17:51:10 -0600

    Add libraries, links, images

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    
+<i class="far fa-hand-rock"></i>
+<i class="far fa-hand-paper"></i>
+<i class="far fa-hand-scissors"></i>
+
+
+var config = {
+    apiKey: "AIzaSyC6NBhyPfBluqpvMCw-C5NMQlfAJmzhVKw",
+    authDomain: "inconceivable-wits.firebaseapp.com",
+    databaseURL: "https://inconceivable-wits.firebaseio.com",
+    projectId: "inconceivable-wits",
+    storageBucket: "",
+    messagingSenderId: "582818727826"
+  };
+  firebase.initializeApp(config);
