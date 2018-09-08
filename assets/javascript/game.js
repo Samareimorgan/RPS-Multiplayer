2575cb50d5c77529bd77468e42355ac638bcaf80
Samantha <samareimorgan@gmail.com>
2018-09-08 15:56:51 -0600

    Add firebase

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,49 @@
-$(document).ready(function () {
+//$(document).ready(function () {
 
-  /*<i class="far fa-hand-rock"></i>
-  <i class="far fa-hand-paper"></i>
-  <i class="far fa-hand-scissors"></i> */
+//Initialize Firebase-->
+var config = {
+  // ...
+};
+firebase.initializeApp(config);
 
-  //Firebase Initialization-----
+$("#googleSignIn").on("click", function () {
+  googleSignIn();
+},
 
-  var config = {
-    apiKey: "AIzaSyC6NBhyPfBluqpvMCw-C5NMQlfAJmzhVKw",
-    authDomain: "inconceivable-wits.firebaseapp.com",
-    databaseURL: "https://inconceivable-wits.firebaseio.com",
-    projectId: "inconceivable-wits",
-    storageBucket: "",
-    messagingSenderId: "582818727826"
-  };
-  firebase.initializeApp(config);
 
-  ///Variables ------
+
+  // Initialize the FirebaseUI Widget using Firebase.
+
+  function googleSignIn() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      console.log(result);
+      console.log("Success..Google Authorization Complete")
+      var token = result.credential.accessToken;
+      console.log(token);
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);
+      // ...
+    }).catch(function (error) {
+      console.log(error);
+      console.log("Failed to Authenticate");
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  })
+
+
+
+ /* ///Variables ------
   var database = firebase.database;
   var connectionsRef = database.ref("/connections");
   var connectedRef = database.ref("info/connected");
@@ -25,59 +52,65 @@ $(document).ready(function () {
 
 
   //Functions ----------
-  /*function signIn() {
-    // Sign in Firebase using popup auth and Google as the identity provider.
+  $(document).on("load", function () {
+    signIn();
+  },
+
+
+  function signIn() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider);
+  },
       // Signs-out of Friendly Chat.
   function signOut() {
     // Sign out of Firebase.
     firebase.auth().signOut();
+  },
 // Initiate firebase auth.
-function initFirebaseAuth() {
+  function initFirebaseAuth() {
   // Listen to auth state changes.
   firebase.auth().onAuthStateChanged(authStateObserver);
-}
+},
 // Returns the signed-in user's profile Pic URL.
-function getProfilePicUrl() {
+  function getProfilePicUrl() {
   return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
-}
+},
 
 // Returns the signed-in user's display name.
-function getUserName() {
+  function getUserName() {
   return firebase.auth().currentUser.displayName;
-}
+},
 
 // Returns true if a user is signed-in.
-function isUserSignedIn() {
+  function isUserSignedIn() {
   return !!firebase.auth().currentUser;
-}
+},
 
 // Loads chat messages history and listens for upcoming ones.
-function loadMessages() {
-  // Loads the last 12 messages and listen for new ones.
-  var callback = function(snap) {
-    var data = snap.val();
-    displayMessage(snap.key, data.name, data.text, data.profilePicUrl, data.imageUrl);
-  };
+  function loadMessages() {
+  // Loads the last 3 messages and listen for new ones.
 
-  firebase.database().ref('/messages/').limitToLast(12).on('child_added', callback);
-  firebase.database().ref('/messages/').limitToLast(12).on('child_changed', callback);
-}
+  firebase.database().ref('/messages/').limitToLast(3).on('child_added', callback);
+  firebase.database().ref('/messages/').limitToLast(3).on('child_changed', callback);
+},
 
 // Saves a new message on the Firebase DB.
-function saveMessage(messageText) {
+  function saveMessage(messageText) {
   // Add a new message entry to the Firebase Database.
-  return firebase.database().ref('/messages/').push({
-    name: getUserName(),
-    text: messageText,
-    profilePicUrl: getProfilePicUrl()
-  }).catch(function(error) {
-    console.error('Error writing new message to Firebase Database', error);
-  });
-  */
+    return firebase.database().ref('/messages/').push({
+      name: getUserName(),
+      text: messageText,
+      profilePicUrl: getProfilePicUrl()
+    }).catch(function(error) {
+      console.error('Error writing new message to Firebase Database', error);
+    })
+  })
+})
+
 
   // Main Process -----
+
   /* On document load the the characters are randomly selected in the left and right columns 
   above the player name.
   //When someone goes to the website, they are asked to enter their name.  
@@ -87,7 +120,5 @@ function saveMessage(messageText) {
   The name then appears one of photos of either Wesley or Vizinni.   And an announcement says "you are playing...vizinni or dread pirate roberts"
   // 
 
+*/
 
-
-
-})
---
+++ assets/javascript/game2.js
@@ -0,0 +1,144 @@
+$(document).ready(function() {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC6NBhyPfBluqpvMCw-C5NMQlfAJmzhVKw",
+    authDomain: "inconceivable-wits.firebaseapp.com",
+    databaseURL: "https://inconceivable-wits.firebaseio.com",
+    projectId: "inconceivable-wits",
+    storageBucket: "inconceivable-wits.appspot.com",
+    messagingSenderId: "582818727826"
+  };
+  firebase.initializeApp(config);
+
+  $("#launchBtn").on("click", function() {
+    $("#challengeVid").attr("src", "https://www.youtube.com/embed/EZSx3zNZOaU?rel=0&amp;autoplay=1&amp;controls=0&amp;showinfo=0&amp;start=38&end=65");
+  });
+  
+  $("#playBtn").on("click", function() {
+    $("#launchBtn").hide();
+    characterChoice();
+
+    
+  })
+
+ 
+  var characters = ["Dread Pirate Roberts", "Vizzini"];
+  var selectCharacter = characters[Math.floor(Math.random()*characters.length)];
+
+  function characterChoice() {
+    if (selectCharacter ==="Dread Pirate Roberts") {
+      $("#dprPlayer").html("#displayName")
+    }
+      else 
+       $("#displayName").html("#vizzini")
+  }
+
+  
+  firebase.database().ref("Messages").on('value', function(dataSnapshot) {
+  
+    console.log(dataSnapshot.val());
+    $("#messages").empty();
+    $(dataSnapshot.val()).each(function(index, message) {
+      // Use jquery to make a single message div
+      // append name p to message div with jquery
+      // Use jquery to add a p for text inside the message dive
+      // same with time
+      // append message div $('#messages'):
+    });
+  
+  });
+  
+  
+  /*function signIn() {
+    // Sign in Firebase using popup auth and Google as the identity provider.
+    var provider = new firebase.database();
+    firebase.auth().signInWithPopup(provider);
+  },
+          // Signs-out of Friendly Chat.
+      function signOut() {
+        // Sign out of Firebase.
+        firebase.auth().signOut();
+      },
+    // Initiate firebase auth.
+      function initFirebaseAuth() {
+      // Listen to auth state changes.
+      firebase.auth().onAuthStateChanged(authStateObserver);
+    },
+    // Returns the signed-in user's profile Pic URL.
+      function getProfilePicUrl() {
+      return firebase.auth().currentUser.photoURL || '/images/profile_placeholder.png';
+    },
+    
+    // Returns the signed-in user's display name.
+      function getUserName() {
+      return firebase.auth().currentUser.displayName;
+    },
+    
+    // Returns true if a user is signed-in.
+      function isUserSignedIn() {
+      return !!firebase.auth().currentUser;
+    },
+    
+    // Loads chat messages history and listens for upcoming ones.
+      function loadMessages() {
+      // Loads the last 3 messages and listen for new ones.
+    
+      firebase.database().ref('/messages/').limitToLast(3).on('child_added', callback);
+      firebase.database().ref('/messages/').limitToLast(3).on('child_changed', callback);
+    },
+    
+    // Saves a new message on the Firebase DB.
+      function saveMessage(messageText) {
+      // Add a new message entry to the Firebase Database.
+        return firebase.database().ref('/messages/').push({
+          name: getUserName(),
+          text: messageText,
+          profilePicUrl: getProfilePicUrl()
+        }).catch(function(error) {
+          console.error('Error writing new message to Firebase Database', error);
+        })
+
+  })
+
+});
+
+/* Pseudo Code -------------------------------------------------
+Launch New Game Function
+   Empties both player names
+   Empties the wins and losses for each player
+   Signs out of firebase connected
+Let's Play Function 
+  Randomly Chooses which character they will play: Dread Pirate Roberts or Vizzini the Sicilian
+     if Dread Pirate Roberts already has a display name, it will default to Vizzini the Sicilian
+  Loads the display name in the correct area
+     If DPR is choosen, a message will state: "Will you outwit the Sicilian and save Princess Buttercup?" 
+     If Vizzini is chosen, a message will state: "Who is this masked man? He is no match for a Sicilian
+     
+Game Function
+ If Rock vs Scissors, DRP wins++, Vizzini losses++
+  DPR win modal triggered...now chose your next weapon
+ If Rock vs Paper, DRP losses++, Vizzini wins++
+  Vizzini win modal triggered...now chose your next weapon
+ If Rock vs Rock, 
+  No one wins modal triggered...now chose your next weapon
+
+Game Stop Function
+  Once DPR wins + losses = 10 
+  Game Over Modal shows
+  If DPR wins > Vizzini wins = YOU TUBE Vizzini dies occurs
+  If DPR wins < Vizzini wins = YOU TUBE WESLEY in the pit of despair
+
+On Close btn of Game Over Modal =  Launch New Game Button Shows
+
+
+The CHALLENGE VIDEO = <iframe width="300" height="200" src="https://www.youtube.com/embed/EZSx3zNZOaU?rel=0&amp;controls=0&amp;showinfo=0&amp;start=38&end=65" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+
+DPR Loses Video =<iframe width="300" height="200" src="https://www.youtube.com/embed/JFo6iLDNzX0?rel=0&amp;controls=0&amp;showinfo=0&amp;start=122&end=135" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+
+Vizzini Loses Video = <iframe width="300" height="200" src="https://www.youtube.com/embed/EZSx3zNZOaU?rel=0&amp;controls=0&amp;showinfo=0&amp;start=256&end=286" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+ */
+  })
+     
